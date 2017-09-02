66eb6adaa9501a2e381c8db5c481407818ca489d
gblanc5 <gregoire.blanc@heig-vd.ch>
2017-09-02 15:33:54 +0200

    change param for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
     address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'sendgrid.com',
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
--- lib/tasks/alert.rake
+++ lib/tasks/alert.rake
@@ -3,7 +3,7 @@ namespace :alert do
     task send: :environment do    
         alerts = Givemealert.where("sendtodate >= ? AND sendtodate < ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day) 
         alerts.each do |alert|
-           UserMailer.send_email(alert).deliver_now
+           UserMailer.send_email(alert).deliver
            puts "An alert: #{alert.sendtowho}"
         end
         
