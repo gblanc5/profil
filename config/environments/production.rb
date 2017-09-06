9b6ff6accade36bbc293c019191e5510efd7973f
gblanc5 <gregoire.blanc@heig-vd.ch>
2017-09-06 12:09:27 +0200

    change esp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,9 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
+    address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'sendgrid.com',
+    domain:               'gmail.com',
     user_name:            ENV["EMAIL_USERNAME"],
     password:             ENV["EMAIL_PASSWORD"],
     authentication:       'plain',
